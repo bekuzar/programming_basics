c159b6d428702d703a936abc0691f3e384ff4a6e
Олисаев Эльбрус Георгиевич <EG.Olisaev@dc.nosu>
2025-05-30 10:26:33 +0300

    Commit: validation

--- pyqt_project/script.py
+++ pyqt_project/script.py
@@ -1,12 +1,34 @@
 from PySide6.QtWidgets import QApplication
 from PySide6 import QtWidgets
 import sys
-from KhechPyside import Ui_MainWindow
+from main_window import Ui_MainWindow
+
 
 class MainWindow(QtWidgets.QMainWindow,Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.bt_confirm.clicked.connect(self.save_user)
+    
+    def save_user(self):
+        if self.validate():
+            pass  # сохраняем в файл
+        else:
+            
+    def validate(self):
+        if self.ed_username.text.length == 0:
+            return False
+        if self.ed_password.text < 8:
+            return False
+        if self.ed_password.text != self.ed_confirm_pass.text:
+            return False
+        
+        return True
+    
+    
+    
+        
+    
 
 
 app = QApplication(sys.argv)
